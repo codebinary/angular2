5c5fb332918d58a1e2488467d5831bb24f9d68e1
James Ontiveros <james.ontiveros86@gmail.com>
2017-02-05 19:36:32 -0500

    Creando menu

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -11,12 +11,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Hello {{name}}</h1><router-outlet></router-outlet>",
+            templateUrl: "app/view/layout.html",
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1><router-outlet></router-outlet>`,
+  templateUrl: `app/view/layout.html`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { }
